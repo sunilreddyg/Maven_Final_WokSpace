3e8c151d570dd464ff00f6a47f67e3d20675c5a2
sunilreddyG1 <sunny.user2@rediffmail.com>
2021-04-22 19:42:31 +0530

    Create open and close browser methods

---
+++ Project_A_123/src/test/java/p4/ClassD.java
@@ -0,0 +1,18 @@
+package p4;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassD {
+  @Test
+  public void openBrowser() 
+  {
+	  Reporter.log("Browser is opened");
+  }
+  
+  @Test
+  public void closebrowser() 
+  {
+	  Reporter.log("close browser");
+  }
+}
