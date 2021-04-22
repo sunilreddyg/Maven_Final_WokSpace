65824f0b7e91e971af0f6a4836c0e269419ebfa9
sunilreddyG1 <sunny.user2@rediffmail.com>
2021-04-22 19:38:03 +0530

    Change method names in ClassC file

--- Project_A_123/src/test/java/p3/ClassC.java
+++ Project_A_123/src/test/java/p3/ClassC.java
@@ -3,10 +3,17 @@ package p3;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class ClassC {
-  @Test
-  public void finaltestMethod() 
+public class ClassC 
+{
+  @Test(priority=0)
+  public void firstTest() 
   {
-	  Reporter.log("Test method finished");
+	  Reporter.log("first test method");
+  }
+  
+  @Test
+  public void SecondTest()
+  {
+	  Reporter.log("Second test finishded");
   }
 }
